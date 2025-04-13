0a4441dd7d34669406ee623eac577526ce9c48ee
YusufXojimurodov <xojimurodovyusufjon2@gmail.com>
2025-04-13 22:29:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let ism = prompt("Ismingizni kiriting")
+let id = prompt("Space id'yngizni kiriting")
+let password = prompt("Sapce passwordingizni kiriting")
+
+const obj = {
+    names: ism,
+    id: id,
+    parol: password,
+}
+
+console.log(obj);
+
